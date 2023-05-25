016bc2041054c57fe87c50af37fbfe31a4a50c95
Sammy <sammyabubakr@gmail.com>
2023-05-25 11:57:09 -0500

    Fixed script.j:7 'Math error fix'

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ function calculateBMI() {
     let showOutput = document.getElementById('output');
     let classification = '';
 
-    let bmiRange = weight (height / 100) ** 2;
+    let bmiRange = weight / (height / 100) ** 2;
     bmiRange = bmiRange.toFixed(2);
+
+    if (isNaN(bmiRange)) {
+        showOutput.innerHTML = 'Invalid input. Please enter valid height and weight.';
+        return;
+    }
 
     if (bmiRange < 16) {
         classification = 'Severe Thinness';
-    } else if (bmiRange >= 16 && bmiRange < 17) {
+    } else if (bmiRange < 17) {
         classification = 'Moderate Thinness';
-    } else if (bmiRange >= 17 && bmiRange < 18.5) {
+    } else if (bmiRange < 18.5) {
         classification = 'Mild Thinness';
-    } else if (bmiRange >= 18.5 && bmiRange < 25) {
+    } else if (bmiRange < 25) {
         classification = 'Normal';
-    } else if (bmiRange >= 25 && bmiRange < 30) {
+    } else if (bmiRange < 30) {
         classification = 'Overweight';
-    } else if (bmiRange >= 30 && bmiRange < 34) {
+    } else if (bmiRange < 34) {
         classification = 'Obese Class I';
-    } else if (bmiRange >= 34 && bmiRange < 40) {
+    } else if (bmiRange < 40) {
         classification = 'Obese Class II';
-    } else if (bmiRange >= 40) {
+    } else {
         classification = 'Obese Class III';
     }
     showOutput.innerHTML = `BMI Range: ${bmiRange} | ${classification} ` ;
+    return;
 }
